aa25b7a496b2763eff37863d5051ee1555b9cf97
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-07-12 13:56:56 +0300

    init

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+}
+
+module.exports = config
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="hello-world">Hello World!</div>
+}
+
+export default App
