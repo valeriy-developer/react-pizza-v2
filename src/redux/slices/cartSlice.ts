44bafc5eb194646451ecdd731f9716133282cdf7
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-08-31 18:11:33 +0300

    fix: total price in cart

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -29,6 +29,10 @@ export const cartSlice = createSlice({
       if (findItem) {
         findItem.count++
       }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count + sum
+      }, 0)
     },
     minusItem(state, action: PayloadAction<string>) {
       const findItem = state.items.find(obj => obj.id === action.payload)
@@ -36,6 +40,10 @@ export const cartSlice = createSlice({
       if (findItem) {
         findItem.count--
       }
+
+      state.totalPrice = state.items.reduce((sum, obj) => {
+        return obj.price * obj.count - sum
+      }, 0)
     },
     removeItem(state, action: PayloadAction<string>) {
       state.items = state.items.filter(obj => obj.id !== action.payload)
