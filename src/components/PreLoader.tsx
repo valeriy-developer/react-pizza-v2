6fad5a5174e7bf7fad30786595215539e8aa0cba
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-09-25 13:07:09 +0300

    feat: add preloader

---
+++ src/components/PreLoader.tsx
@@ -0,0 +1,82 @@
+const Preloader: React.FC = () => {
+  return (
+    <div className="preloader">
+      <div className="box">
+        <div className="pizza-slice slice-1">
+          <div className="border">
+            <div className="crust" />
+            <div className="cheese">
+              <div className="peperoni p-1" />
+              <div className="peperoni p-2" />
+              <div className="peperoni p-3" />
+              <div className="olive o-1" />
+              <div className="olive o-3" />
+              <div className="olive o-4" />
+              <div className="olive o-6" />
+              <div className="olive o-7" />
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-2">
+          <div className="border">
+            <div className="crust" />
+            <div className="cheese">
+              <div className="peperoni p-1" />
+              <div className="peperoni p-2" />
+              <div className="peperoni p-3" />
+              <div className="olive o-1" />
+              <div className="olive o-2" />
+              <div className="olive o-3" />
+              <div className="olive o-6" />
+              <div className="olive o-7" />
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-3">
+          <div className="border">
+            <div className="crust" />
+            <div className="cheese">
+              <div className="peperoni p-1" />
+              <div className="peperoni p-2" />
+              <div className="peperoni p-3" />
+              <div className="olive o-3" />
+              <div className="olive o-4" />
+              <div className="olive o-5" />
+              <div className="olive o-6" />
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-4">
+          <div className="border">
+            <div className="crust" />
+            <div className="cheese">
+              <div className="peperoni p-1" />
+              <div className="peperoni p-2" />
+              <div className="peperoni p-3" />
+              <div className="olive o-1" />
+              <div className="olive o-2" />
+              <div className="olive o-3" />
+              <div className="olive o-4" />
+            </div>
+          </div>
+        </div>
+        <div className="pizza-slice slice-5">
+          <div className="border">
+            <div className="crust" />
+            <div className="cheese">
+              <div className="peperoni p-1" />
+              <div className="peperoni p-2" />
+              <div className="peperoni p-3" />
+              <div className="olive o-1" />
+              <div className="olive o-2" />
+              <div className="olive o-6" />
+              <div className="olive o-7" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Preloader
